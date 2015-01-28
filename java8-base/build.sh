eadff43b122cdffd7ceafd387e91d63884bfc371
lexandro <lexandro2000@gmail.com>
2015-01-28 07:25:47 +0100

    Moving java download from script to Dockerfile to enable hub automated build

--- java8-base/build.sh
+++ java8-base/build.sh
@@ -1,7 +1,7 @@
 
-if [ ! -f jdk-8u25-linux-x64.tar.gz  ]; then
-    curl -j -k -L -H "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u25-b17/jdk-8u25-linux-x64.tar.gz>jdk-8u25-linux-x64.tar.gz
-fi
+#if [ ! -f jdk-8u25-linux-x64.tar.gz  ]; then
+#    curl -j -k -L -H "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u31-b13/jdk-8u31-linux-x64.tar.gz>jdk-8u31-linux-x64.tar.gz
+#fi
 
 docker build --force-rm=true -t lexandro/java8-base .
 
